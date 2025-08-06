3db55b3435aedb8300ebf67d5277c937bfed0365
Eduardo Olea <134759453+OleaRosasE@users.noreply.github.com>
2025-08-05 21:01:52 -0500

    Adicion del menu responsivo prueba 1

--- index.js
+++ index.js
@@ -1,60 +1,68 @@
-const formulario = document.getElementById("contact-form");
-const mensaje = document.getElementById("form-message");
-const secciones = document.querySelectorAll("section");
-const enlaces = document.querySelectorAll(".nav-link");
+document.addEventListener("DOMContentLoaded", function () {
+    const formulario = document.getElementById("contact-form");
+    const mensaje = document.getElementById("form-message");
+    const secciones = document.querySelectorAll("section");
+    const enlaces = document.querySelectorAll(".nav-link");
+    const menuResposive = document.querySelector(".menu-res");
 
-window.addEventListener("scroll", () => {
+    menuResposive.onclick = function () {
+        navbar = document.querySelector(".menu-nav");
+        navbar.classList.toggle("active");
+    };
 
-    let actual = "";
+    window.addEventListener("scroll", () => {
 
-    secciones.forEach(section => {
-        const seccionTop = section.offsetTop;                           //Obtener la seccion superior respecto del documento
-        const seccionHeight = section.offsetHeight;                 //Obtener la altura de la seccion
+        let actual = "";
 
-        if (pageYOffset >= seccionTop - seccionHeight / 3) {    //Evaluar si se ha alcanzado 1/3 de la seccion actual
-            actual = section.getAttribute("id");                            //Guardar la etiqueta actual en la variable actual
-        }
-    });
+        secciones.forEach(section => {
+            const seccionTop = section.offsetTop;                           //Obtener la seccion superior respecto del documento
+            const seccionHeight = section.offsetHeight;                 //Obtener la altura de la seccion
 
-    enlaces.forEach(link => {
-        link.classList.remove("active");
-        if (link.getAttribute("href") === `#${actual}`) {               //Comparamos si el enlace coincide con el id de la seccion
-            link.classList.add("active");
-        }
-    });
-});
-
-
-
-formulario.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const datos = new FormData(formulario);
-
-    try {
-        const respuesta = await fetch(formulario.action, {
-            method: "POST",
-            body: datos,
-            headers: {
-                Accept: "application/json"
+            if (pageYOffset >= seccionTop - seccionHeight / 3) {    //Evaluar si se ha alcanzado 1/3 de la seccion actual
+                actual = section.getAttribute("id");                            //Guardar la etiqueta actual en la variable actual
             }
         });
 
-        if (respuesta.ok) {
-            mensaje.style.display = "block";
-            mensaje.textContent = "Gracias por contactarme, te escribiré pronto";
-            formulario.reset();
+        enlaces.forEach(link => {
+            link.classList.remove("active");
+            if (link.getAttribute("href") === `#${actual}`) {               //Comparamos si el enlace coincide con el id de la seccion
+                link.classList.add("active");
+            }
+        });
+    });
 
-        } else {
+
+
+    formulario.addEventListener("submit", async (e) => {
+        e.preventDefault();
+        const datos = new FormData(formulario);
+
+        try {
+            const respuesta = await fetch(formulario.action, {
+                method: "POST",
+                body: datos,
+                headers: {
+                    Accept: "application/json"
+                }
+            });
+
+            if (respuesta.ok) {
+                mensaje.style.display = "block";
+                mensaje.textContent = "Gracias por contactarme, te escribiré pronto";
+                formulario.reset();
+
+            } else {
+                mensaje.style.display = "block";
+                mensaje.style.color = "red";
+                mensaje.textContent = "Ocurrió un error, por favor intentalo una vez más";
+            }
+            setTimeout(() => {
+                mensaje.style.display = "none";
+            }, 15000); //Ocultar el mensaje tras 5 segundos
+        } catch (error) {
             mensaje.style.display = "block";
             mensaje.style.color = "red";
             mensaje.textContent = "Ocurrió un error, por favor intentalo una vez más";
         }
-        setTimeout(() => {
-            mensaje.style.display = "none";
-        }, 15000); //Ocultar el mensaje tras 5 segundos
-    } catch (error) {
-        mensaje.style.display = "block";
-        mensaje.style.color = "red";
-        mensaje.textContent = "Ocurrió un error, por favor intentalo una vez más";
-    }
+    });
 });
